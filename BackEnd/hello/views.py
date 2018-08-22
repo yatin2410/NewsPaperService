d0c03d7d82d7b684afe0f25b95cea158b1ec3f36
YatinPatel <yatinpatel.gt@gmail.com>
2018-08-23 04:46:44 +0530

    added db gs and dab

--- BackEnd/hello/views.py
+++ BackEnd/hello/views.py
@@ -107,6 +107,135 @@ def sandeshsur(request):
 	return HttpResponse('<h1>Done<h1>')
 
 
+def scrapedb(city,code):
+    main_url = "http://digitalimages.bhaskar.com/gujarat/epaperpdf/"
+    if time.localtime(time.time()).tm_mon<10 :
+        month = '0'+str(time.localtime(time.time()).tm_mon)
+    else :
+        month = time.localtime(time.time()).tm_mon;
+    if time.localtime(time.time()).tm_mday<10 :
+        date = '0'+str(time.localtime(time.time()).tm_mday)
+    else :
+        date = time.localtime(time.time()).tm_mday;
+    pdfs = []
+    id = str(date)+str(month)+'2018'+'/'+str(date-1)+city+'%20CITY-PG'
+    merger = PdfFileMerger()
+    for i in range(1, 35):
+        try:
+            back_url = str(id)+str(i)+str('-0')+'.PDF'
+            print(main_url+back_url)
+            respose =  urlopen(main_url+str(back_url))
+            pagename = 'temp'+str(i)+'.pdf'
+            fl = open(pagename, 'wb')
+            fl.write(respose.read())
+            fl.close()
+            pdfs.append(pagename)
+        except:
+            print(pdfs)
+            for pdf in pdfs:
+                print(pdf)
+                merger.append(pdf)
+            merger.write("db"+code+".pdf")
+            break
+
+def dbahm(request):
+    scrapedb('AHMEDABAD','ahm')
+    return HttpResponse('<h1>Done<h1>')
+
+def dbbar(request):
+    scrapedb('BARODA','bar')
+    return HttpResponse('<h1>Done<h1>')
+
+def dbraj(request):
+    scrapedb('RAJKOT','raj')
+    return HttpResponse('<h1>Done<h1>')
+
+def dbsur(request):
+    scrapedb('SURAT','sur')
+    return HttpResponse('<h1>Done<h1>')
+
+def scrapegs(code):
+    name = code.upper()
+    main_url = "http://gujaratsamacharepaper.com/download.php?file=http://enewspapr.com/News/GUJARAT/"+name+"/"
+    if time.localtime(time.time()).tm_mon<10 :
+        month = '0'+str(time.localtime(time.time()).tm_mon)
+    else :
+        month = time.localtime(time.time()).tm_mon;
+    if time.localtime(time.time()).tm_mday<10 :
+        date = '0'+str(time.localtime(time.time()).tm_mday)
+    else :
+        date = time.localtime(time.time()).tm_mday;
+    pdfs = []
+    id = '2018'+str(month)+str(date)+'_'
+    merger = PdfFileMerger()
+    for i in range(1, 35):
+        back_url = '2018' + '/'+str(month)+'/'+str(date)+'/'+str(id)+str(i)+'.PDF'
+        print(main_url+back_url)
+        respose = urlopen(main_url+str(back_url))
+        pagename = 'temp'+str(i)+'.pdf'
+        pdfs.append(pagename)
+        fl = open(pagename, 'wb')
+        fl.write(respose.read())
+        fl.close()
+        if os.stat('temp'+str(i)+'.pdf').st_size > 0:
+            merger.append('temp'+str(i)+'.pdf')
+    merger.write("gs"+code+".pdf")
+
+def gsahm(request):
+    scrapegs("ahm")
+    return HttpResponse('<h1>Done<h1>')
+
+def gsbar(request):
+    scrapegs("bar")
+    return HttpResponse('<h1>Done<h1>')
+
+def gsraj(request):
+    scrapegs("raj")
+    return HttpResponse('<h1>Done<h1>')
+
+def gssur(request):
+    scrapegs("sur")
+    return HttpResponse('<h1>Done<h1>')
+
+
+def scrapedab(main_url,idd,code):
+    if time.localtime(time.time()).tm_mon<10 :
+        month = '0'+str(time.localtime(time.time()).tm_mon)
+    else :
+        month = time.localtime(time.time()).tm_mon;
+    if time.localtime(time.time()).tm_mday<10 :
+        date = '0'+str(time.localtime(time.time()).tm_mday)
+    else :
+        date = time.localtime(time.time()).tm_mday;
+    pdfs = []
+    id = str(date)+str(month)+'2018'+'/'+str(date-1)+idd
+    merger = PdfFileMerger()
+    for i in range(1, 35):
+        try:
+            back_url = str(id)+str(i)+str('-0')+'.PDF'
+            print(main_url+back_url)
+            respose = urlopen(main_url+str(back_url))
+            pagename = 'temp'+str(i)+'.pdf'
+            pdfs.append(pagename)
+            fl = open(pagename, 'wb')
+            fl.write(respose.read())
+            fl.close()
+        except:
+            for pdf in pdfs:
+                merger.append(pdf)
+            merger.write("dab"+code+".pdf")
+            break
+
+def dabsur(request):
+    scrapedab("http://digitalimages.bhaskar.com/mpcg/epaperpdf/",'SURAT%20CITY%20HINDI-PG','sur')
+    return HttpResponse('<h1>Done<h1>')
+
+def dabdel(request):
+    scrapedab("http://digitalimages.bhaskar.com/cph/epaperpdf/",'DELHI%20CITY-PG','del')
+    return HttpResponse('<h1>Done<h1>')
+
+
+
 def scrapeother(main_url,code):
 	req = Request(main_url,headers={'User-Agent': 'Mozilla/5.0'})
 	page_html = urlopen(req).read()
@@ -184,6 +313,19 @@ def sendnews(code):
 def sendnewssandesh(request):
 	sendnews('sandesh')
 	return HttpResponse('<h1> send </h1>')
+
+def sendnewsdb(request):
+    sendnews('db')
+    return HttpResponse('<h1> send </h1>')
+
+
+def sendnewsgs(request):
+    sendnews('gs')
+    return HttpResponse('<h1> send </h1>')
+
+def sendnewsdab(request):
+    sendnews('dab')
+    return HttpResponse('<h1> send </h1>')
 
 def sendnewsother(request):
 	sendnews('tie')
